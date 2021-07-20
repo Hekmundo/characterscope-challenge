6da69db70610379889a9900f5102f8015f633d65
Alex Hek <alexanderhek@gmail.com>
2021-07-21 00:11:13 +0100

    changed Hero image size to reduce on screens of width of 320px or less

--- src/components/hero/hero.styles.js
+++ src/components/hero/hero.styles.js
@@ -100,7 +100,7 @@ export const HiddenImage = styled.img`
     opacity: 1;
   }
 
-  @media screen and (max-width: 360px) {
+  @media screen and (max-width: 320px) {
     display: none;
   }
 `;
@@ -109,7 +109,6 @@ export const HeroContainer = styled.div`
   height: 100%;
   ${containerStyles}
   background-color: #f8f8f8;
-  /* position: relative; */
 `;
 
 export const Title = styled.h1`
@@ -151,7 +150,7 @@ export const HeroImage = styled.img`
     }
   }
 
-  @media screen and (max-width: 360px) {
+  @media screen and (max-width: 320px) {
     width: 200px;
   }
 `;
